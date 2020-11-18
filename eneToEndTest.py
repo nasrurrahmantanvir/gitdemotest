f7ce0b9b97db8544b1c8885a0038d5102fa47fa8
Mr-Tanvir <tanvir.zob@gmail.com>
2020-11-18 21:36:45 +0600

    endtoend

---
+++ eneToEndTest.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path="C:\\Users\Mr-Tanvir\PycharmProjects\chromedriver.exe" )
+driver.get("https://rahulshettyacademy.com/angularpractice/")
+driver.maximize_window()
+driver.implicitly_wait(8)
+
+driver.find_element_by_css_selector("a[href*='shop']").click()
+products = driver.find_elements_by_xpath("//div[@class='card h-100']")
+#//div[@class='card h-100']/div/h4/a
+
+for product in products:
+    productName = product.find_element_by_xpath("div/h4/a").text
+    if productName == 'Blackberry':
+        product.find_element_by_xpath("div/button").click()
+
+driver.find_element_by_css_selector("a[class='nav-link btn btn-primary']").click()
+time.sleep(1)
+driver.find_element_by_xpath("//button[@class='btn btn-success']").click()
+time.sleep(1)
+driver.find_element_by_id("country").send_keys("Bangladesh")
+driver.find_element_by_link_text('Bangladesh').click()
+driver.find_element_by_xpath("//div[@class='checkbox checkbox-primary']").click()
+time.sleep(1)
+driver.find_element_by_css_selector("[type='submit']").click()
+assert driver.find_element_by_xpath("//div[@class='alert alert-success alert-dismissible']/strong").text == 'Success!'
+
+
+
+
+
